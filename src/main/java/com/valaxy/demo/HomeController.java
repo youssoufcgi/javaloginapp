843d39030cd3e10c83ec671c9dc30aca01a4a9c0
hibamoumneh <hiba.moumneh@gmail.com>
2022-02-06 10:01:19 -0500

    dasd

--- src/main/java/com/valaxy/demo/HomeController.java
+++ src/main/java/com/valaxy/demo/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.apache.log4j.Logger;
+// import org.apache.log4j.Logger;
 
 @Controller
 
--- src/test/java/com/valaxy/demo/MyWebAppApplicationTests.java
+++ src/test/java/com/valaxy/demo/MyWebAppApplicationTests.java
@@ -2,7 +2,7 @@ package com.krishna.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.apache.log4j.Logger;
+// import org.apache.log4j.Logger;
 
 @SpringBootTest
 class MyWebAppApplicationTests {
